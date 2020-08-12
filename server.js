4ceb165b87429505dd4f30286f0c21600089af2e
anushka.singh <anushkasingh.1596@gmail.com>
2020-08-12 20:21:22 +0530

    final commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db')
 const app = express();
+const path = require('path')
 
 //Connect Database
 connectDB();
@@ -18,6 +19,14 @@ app.get('/' , (req, res) =>
 app.use('/api/users', require('./Routes/users'))
 app.use('/api/auth', require('./Routes/auth'))
 app.use('/api/contacts', require('./Routes/contacts'))
+
+//Serve static assets in production
+
+if(process.env.NODE_ENV === 'production'){
+  //Set static folder
+  app.use(express.static('client/build'))
+  app.get('*', (req, res)=>res.sendFile(path.resolve(__dirname, 'client','build', 'index.html' )))
+}
 
 const PORT = process.env.PORT || 5000;
 
